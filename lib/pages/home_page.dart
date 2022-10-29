abd034a223a95afeba9f299269ac62e5e138ff21
bm1987 <b7724887@gmail.com>
2022-10-29 20:33:59 +0500

    PDP UI
    Signed-off-by: BMN1987 <110631069+Baxtiyor87@users.noreply.github.com>

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+class HomePage extends StatefulWidget {
+  static String id = 'home_page';
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      appBar: AppBar(
+       title: const Text('UI'),
+        centerTitle: true,
+      ),
+      body:  Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            Text('User ',style: TextStyle(color: Colors.red, fontSize: 36),),
+             Text('Interface',style: TextStyle(color: Colors.green, fontSize: 36),)
+          ],
+        ),
+      ),
+    );
+  }
+}
